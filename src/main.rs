07460a6ca5315320ead6dbd6e54ec196bcf0664d
Em Sal <absolutezero2a03@gmail.com>
2017-04-28 14:36:43 -0700

    Started main execution.
    Contains file reading and printing, but that's it at the moment.

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::fs::File;
+use std::path::Path;
+use std::io::prelude::*;
+use std::env;
+
+fn main() {
+    let mut x = env::args().nth(1);
+    match x {
+        Some(x) => {
+            let mut file = {
+                let path_x = Path::new(&x);
+                File::open(path_x)
+            };
+
+            match file {
+                Ok(mut f) => {
+                    let mut contents = String::new();
+                    f.read_to_string(&mut contents);
+                    println!("{}", contents)
+                },
+
+                Err(e) => {
+                    println!("{}", e);
+                },
+            }
+        },
+
+        None =>
+            println!("No argument at 1"),
+    }
+}
