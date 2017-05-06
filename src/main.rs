e2fca1c65a66a99a9fea654d036633a7767fdc16
Em Sal <absolutezero2a03@gmail.com>
2017-05-05 23:32:28 -0700

    Added skeleton for video processing.
    Still need to make everything safe (safe interface)

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,71 @@
-use std::fs::File;
-use std::path::Path;
-use std::io::prelude::*;
+use std::ffi::CString;
 use std::env;
 
-fn main() {
-    let mut x = env::args().nth(1);
-    match x {
-        Some(x) => {
-            let mut file = File::open(&x);
+extern crate ffmpeg_sys;
 
-            match file {
-                Ok(mut f) => {
-                    let mut contents = String::new();
-                    f.read_to_string(&mut contents);
-                    println!("{}", contents)
-                },
+fn get_codec_type(mut p_format_ctx: *mut ffmpeg_sys::AVFormatContext) {
+    return;
+}
 
-                Err(e) => {
-                    println!("{}", e);
-                },
+fn process_vid(mut p_format_ctx: *mut ffmpeg_sys::AVFormatContext, x: String) {
+    unsafe {
+        let xcstr = CString::new(x).unwrap();
+
+        //let ctx_ptr = &p_format_ctx
+
+        if ffmpeg_sys::avformat_open_input(
+                &mut p_format_ctx,
+                xcstr.as_ptr(),
+                std::ptr::null_mut(),
+                std::ptr::null_mut()) != 0 {
+
+            println!("Can't open file");
+            return;
+        }
+
+        if ffmpeg_sys::avformat_find_stream_info(
+                p_format_ctx,
+                std::ptr::null_mut()) != 0 {
+
+            println!("No stream info");
+            return;
+        }
+
+        ffmpeg_sys::av_dump_format(p_format_ctx, 0, xcstr.as_ptr(), 0);
+
+        let mut video_stream: i32 = -1;
+        for i in 0..(*p_format_ctx).nb_streams {
+            let stream = *((*p_format_ctx).streams.offset(i as isize));
+            let ref codec_ctx: ffmpeg_sys::AVCodecContext = *((*stream).codec);
+
+            if codec_ctx.codec_type == ffmpeg_sys::AVMEDIA_TYPE_VIDEO {
+
+                video_stream = i as i32;
+                break;
             }
-        },
+        }
 
-        None =>
-            println!("No argument at 1"),
+        if video_stream == -1 {
+            println!("no video stream");
+            return;
+        }
+    }
+}
+
+fn main() {
+    unsafe {
+        ffmpeg_sys::av_register_all();
+        let mut p_format_ctx : *mut ffmpeg_sys::AVFormatContext = std::ptr::null_mut();
+
+        let x = env::args().nth(1);
+
+        match x {
+            Some(x) => {
+                process_vid(p_format_ctx, x);
+            },
+
+            None =>
+                println!("No argument at 1"),
+        }
     }
 }
