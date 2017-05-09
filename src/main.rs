392ab971a6fe258bbc4a9d2a0e4164bf3f434fe7
AbsoluteZero2A03 <AbsoluteZero2A03@gmail.com>
2017-05-09 07:18:23 -0700

    Refactored main function to make it less unsafe
    Serves as a blueprint for the refactoring for process_vid.

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,7 @@ fn process_vid(mut p_format_ctx: *mut ffmpeg_sys::AVFormatContext, x: String) {
     unsafe {
         let xcstr = CString::new(x).unwrap();
 
-        //let ctx_ptr = &p_format_ctx
-
+        // Unsafe function
         if ffmpeg_sys::avformat_open_input(
                 &mut p_format_ctx,
                 xcstr.as_ptr(),
@@ -23,6 +22,7 @@ fn process_vid(mut p_format_ctx: *mut ffmpeg_sys::AVFormatContext, x: String) {
             return;
         }
 
+        // Unsafe
         if ffmpeg_sys::avformat_find_stream_info(
                 p_format_ctx,
                 std::ptr::null_mut()) != 0 {
@@ -31,11 +31,17 @@ fn process_vid(mut p_format_ctx: *mut ffmpeg_sys::AVFormatContext, x: String) {
             return;
         }
 
+        // Unsafe function
         ffmpeg_sys::av_dump_format(p_format_ctx, 0, xcstr.as_ptr(), 0);
 
         let mut video_stream: i32 = -1;
         for i in 0..(*p_format_ctx).nb_streams {
+
+            // Dereference of raw pointer
+            // Invocation of unsafe method
             let stream = *((*p_format_ctx).streams.offset(i as isize));
+
+            //Dereference of raw pointer (2)
             let ref codec_ctx: ffmpeg_sys::AVCodecContext = *((*stream).codec);
 
             if codec_ctx.codec_type == ffmpeg_sys::AVMEDIA_TYPE_VIDEO {
@@ -52,20 +58,24 @@ fn process_vid(mut p_format_ctx: *mut ffmpeg_sys::AVFormatContext, x: String) {
     }
 }
 
-fn main() {
-    unsafe {
+fn get_ffmpeg_context() -> *mut ffmpeg_sys::AVFormatContext {
+    return unsafe {
         ffmpeg_sys::av_register_all();
-        let mut p_format_ctx : *mut ffmpeg_sys::AVFormatContext = std::ptr::null_mut();
+        std::ptr::null_mut()
+    };
+}
 
-        let x = env::args().nth(1);
+fn main() {
+    let mut p_format_ctx : *mut ffmpeg_sys::AVFormatContext = get_ffmpeg_context();
 
-        match x {
-            Some(x) => {
-                process_vid(p_format_ctx, x);
-            },
+    let x = env::args().nth(1);
 
-            None =>
-                println!("No argument at 1"),
-        }
+    match x {
+        Some(x) => {
+            process_vid(p_format_ctx, x);
+        },
+
+        None =>
+            println!("No argument at 1"),
     }
 }
